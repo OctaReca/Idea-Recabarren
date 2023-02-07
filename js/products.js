781bc268c5ce9ebf18b579793c9399480ce8fcbd
OctaReca <115593701+OctaReca@users.noreply.github.com>
2023-02-07 02:37:53 -0300

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Harina",
+        precio: 50,
+        img: "../img/Harina.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Galletas",
+        precio: 100,
+        img: "../img/Galletas.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "Cerveza",
+        precio: 150,
+        img: "../img/Cerveza.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "Leche",
+        precio: 200,
+        img: "../img/Leche.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 5,
+        nombre: "Gaseosa",
+        precio: 250,
+        img: "../img/Gaseosas.jpg",
+        cantidad: 1,
+    },
+];
